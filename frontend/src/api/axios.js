04a0ebffcd5497100458e32060f51c51e5b2a9c6
Munaza Munir <munazaengineering@gmail.com>
2026-08-24 15:16:03 +0500

    Update production backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://complaint-management-system-api.vercel.app/api",
+  baseURL: "https://complaint-management-system-lake.vercel.app/api",
 });
 
 export default API;
