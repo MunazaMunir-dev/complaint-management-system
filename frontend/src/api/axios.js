d4684306e40c682b963800bf139f87c6c6db5399
Munaza Munir <munazaengineering@gmail.com>
2026-08-24 15:58:32 +0500

    Connect frontend to working production backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://complaint-management-system-lake.vercel.app/api",
+  baseURL: "https://complaint-management-system-wine-five.vercel.app/api",
+  withCredentials: true,
 });
 
 export default API;
